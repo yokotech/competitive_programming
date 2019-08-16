5417800512bcdfe10e5079e30b43f7d8e6a39a3c
yokotech <yokoyama-ryo949@g.ecc.u-tokyo.ac.jp>
2019-08-16 20:59:15 +0900

    gcd lib

---
+++ library/gcd_lib/1_gcdlib_a.cpp
@@ -0,0 +1,94 @@
+// 1_gcdlib_a
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>// C++
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+// verify : http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_1_B
+
+using namespace std;
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define rep1(i, n) for(int i = 1; i <= (int)(n); i++)
+#define SZ(x) ((int)(x).size())
+#define INF (1e16)
+typedef long long ll;
+
+int gcd(int x, int y){
+    if(x < y) swap(x, y);
+
+    while(y > 0){
+        int r = x % y;
+        x = y;
+        y = r;
+    }
+    return x;
+}
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    int a, b;
+    cin >> a >> b;
+    cout << gcd(a, b) << endl;
+    return 0;
+}
---
+++ library/gcd_lib/cmake-build-debug/CMakeFiles/1_gcdlib_a.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/1_gcdlib_a.dir/1_gcdlib_a.cpp.o"
+  "1_gcdlib_a.pdb"
+  "1_gcdlib_a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/1_gcdlib_a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
