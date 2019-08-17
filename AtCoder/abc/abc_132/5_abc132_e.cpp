a7c68c242d588ce831e6cc28b1e47d40b18d0218
yokotech <yokoyama-ryo949@g.ecc.u-tokyo.ac.jp>
2019-08-17 10:38:25 +0900

    abc132-E

--- AtCoder/abc/abc_132/5_abc132_e.cpp
+++ AtCoder/abc/abc_132/5_abc132_e.cpp
@@ -73,54 +73,25 @@ typedef long long ll;
 
 int n, m;
 int s, t;
-vector<vector<int>> g;
-vector<int> visited;
-vector<int> d;
-queue<int> q;
-
-void bfs(int nd, int t, int dist){
-    if(t == 3){
-        if(visited[nd] == 0){
-            visited[nd] = 1;
-            d[nd] = dist + 1;
-            q.push(nd);
-        }
-        return;
-    }
-
-    rep(i, g[nd].size()){
-        int next = g[nd][i];
-        bfs(next, t + 1, dist);
-    }
-}
 
 int main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
     cin >> n >> m;
-    g.resize(n + 1);
-    visited.resize(n + 1);
-    d.resize(n + 1);
-    rep1(i, m){
+    vector<vector<vector<int>>> g;
+    vector<vector<int>> d;
+    g.resize(3, vector<vector<int>> (n + 1));
+    d.resize(3, vector<int> (n + 1));
+    rep(i, m){
         int u, v;
         cin >> u >> v;
-        g[u].push_back(v);
-    }
-    rep1(i, n){
-        visited[i] = 0;
-        d[i] = -1;
+        g[0][u].push_back(v);
+        g[1][u].push_back(v);
+        g[2][u].push_back(v);
     }
     cin >> s >> t;
 
-    q.push(s);
-    d[s] = 0;
 
-    while(!q.empty()){
-        int nd = q.front();
-        q.pop();
-        bfs(nd, 0, d[nd]);
-    }
-    cout << d[t] << endl;
 
     return 0;
 }
