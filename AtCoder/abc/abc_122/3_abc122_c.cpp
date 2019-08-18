8fa0ba24be3243012922a61e863d97da1b651e69
yokotech <yokoyama-ryo949@g.ecc.u-tokyo.ac.jp>
2019-08-18 13:49:30 +0900

    abc122-C累積和AC

---
+++ AtCoder/abc/abc_122/3_abc122_c.cpp
@@ -0,0 +1,101 @@
+// 3_abc122_c
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>// C++
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+using namespace std;
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define rep1(i, n) for(int i = 1; i <= (int)(n); i++)
+#define SZ(x) ((int)(x).size())
+#define INF (1e16)
+#define MOD (1000000007)
+typedef long long ll;
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    int n, q;
+    cin >> n >> q;
+    string s;
+    cin >> s;
+
+    vector<int> sum(n + 1);
+    sum[0] = 0;
+    rep1(i, n){
+        if(s.substr(i - 1, 2) == "AC"){
+            sum[i] = sum[i - 1] + 1;
+        }else{
+            sum[i] = sum[i - 1];
+        }
+    }
+
+    rep(i, q){
+        int l, r;
+        cin >> l >> r;
+        l--; r--;
+
+        cout << sum[r] - sum[l] << endl;
+    }
+    return 0;
+}
