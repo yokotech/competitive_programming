1ee43b6aa1a265b1403413bd96de4029f898465e
yokotech <yokoyama-ryo949@g.ecc.u-tokyo.ac.jp>
2019-08-19 22:46:05 +0900

    abc136-E 解説AC

--- AtCoder/abc/abc_136/5_abc136_e.cpp
+++ AtCoder/abc/abc_136/5_abc136_e.cpp
@@ -71,9 +71,56 @@ using namespace std;
 #define INF (1e16)
 typedef long long ll;
 
+int n, k;
+vector<int> a;
+vector<int> r;
+int s;
+int ans;
+
+void func(int d){
+    if(d == 1) return;
+
+    rep(i, n){
+        r[i] = a[i] % d;
+    }
+    sort(r.begin(), r.end());
+    int its = 0;
+    int itt = n - 1;
+    int cnt = 0;
+    while(its < itt){
+        int diff = min(r[its], d - r[itt]);
+        r[its] -= diff;
+        r[itt] += diff;
+        cnt += diff;
+
+        if(r[its] == 0) its++;
+        if(r[itt] == d) itt--;
+    }
+    if(cnt > k) return;
+
+    ans = max(ans, d);
+}
+
 int main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
+    cin >> n >> k;
+    s = 0;
+    ans = 1;
+    a.resize(n);
+    r.resize(n);
+    rep(i, n){
+        cin >> a[i];
+        s += a[i];
+    }
 
+    for(int i = 1; i * i <= s; i++){
+        if(s % i) continue;
+
+        func(i);
+        func(s / i);
+    }
+
+    cout << ans << endl;
     return 0;
 }
