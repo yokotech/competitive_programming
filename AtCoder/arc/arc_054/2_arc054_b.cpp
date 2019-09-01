c67d896ef0aa4f1c80a0ef6327c97a86f9cbcc43
yokotech <yokoyama-ryo949@g.ecc.u-tokyo.ac.jp>
2019-09-01 20:41:20 +0900

    chokudai一昨々日の一問AC

---
+++ AtCoder/arc/arc_054/2_arc054_b.cpp
@@ -0,0 +1,92 @@
+// 2_arc054_b
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>// C++
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+using namespace std;
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define rep1(i, n) for(int i = 1; i <= (int)(n); i++)
+#define SZ(x) ((int)(x).size())
+#define INF (1e16)
+#define MOD (1000000007)
+typedef long long ll;
+
+double p;
+
+double f(long double t){
+    return t + p * pow(2.0, -t / 1.5);
+}
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    cin >> p;
+    double ans = p;
+    double t = -1.5 * log2(1.5 / (p * log(2.0)));
+    if(t > 0 && f(t) > 0 && f(t) < ans) ans = f(t);
+
+    cout << setprecision(15) << ans << endl;
+    return 0;
+}
---
+++ AtCoder/arc/arc_054/cmake-build-debug/CMakeFiles/2_arc054_b.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/2_arc054_b.dir/2_arc054_b.cpp.o"
+  "2_arc054_b.pdb"
+  "2_arc054_b"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2_arc054_b.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
